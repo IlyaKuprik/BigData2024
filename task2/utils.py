3b24af71db0ed50faf7e81fb1cfc85483a94c7cc
9O2wnhwKDg0I <ilyasolonitsyn@mail.ru>
2024-06-27 23:07:25 +0300

    done task 2

---
+++ task2/utils.py
@@ -0,0 +1,16 @@
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+def worker(num, result_list):
+    factors = prime_factors(num)
+    result_list.append(len(factors))
